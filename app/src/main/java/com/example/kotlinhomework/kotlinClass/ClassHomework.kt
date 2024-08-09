40020c7b9a882bfb3f172e519b359f18dfa41de6
Anastasiia Khyshova <a.khyshova@tinkoff.ru>
2024-08-09 14:19:41 +0300

    Add kotlin class homework

---
+++ app/src/main/java/com/example/kotlinhomework/kotlinClass/ClassHomework.kt
@@ -0,0 +1,7 @@
+package com.example.kotlinhomework.kotlinClass
+
+fun main() {
+    val technicalWriter = TechnicalWriter()
+    technicalWriter.haveLunch()
+    technicalWriter.goHome()
+}
---
+++ app/src/main/java/com/example/kotlinhomework/kotlinClass/Employee.kt
@@ -0,0 +1,51 @@
+package com.example.kotlinhomework.kotlinClass
+
+open class Employee {
+
+    fun work() {
+        println("Усердно работаю")
+    }
+
+    open fun haveLunch() {
+        println("Иду на обед в 13.00")
+    }
+
+    open fun goHome() {
+        println("Иду домой в 19.00")
+    }
+}
+
+
+class Programmer() : Employee() {
+    override fun haveLunch() {
+        super.haveLunch()
+        println("А потом еще и за кофеечком")
+    }
+}
+
+
+class Designer : Employee() {
+
+    override fun haveLunch() {
+        println("Иду на обед в 15.00")
+    }
+}
+
+
+class Teamlead : Employee() {
+
+    override fun haveLunch() {
+        println("Сегодня пропущу обед")
+    }
+}
+
+class TechnicalWriter : Employee() {
+
+    override fun goHome() {
+        println("Закончу работать в 18.00")
+    }
+
+    override fun haveLunch() {
+        println("Иду на обед в 14.00 и очень вкусно кушаю")
+    }
+}
